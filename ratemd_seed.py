1a32124f35b065f12d7f6e3e759ec39638656435
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-05-15 22:09:49 +0000

    Updated file to fix variable naming error. Regex in progress and currently now commented out to test results from scrape.

--- ratemd_seed.py
+++ ratemd_seed.py
@@ -21,28 +21,36 @@ for page in range(starting_page,number_of_pages+1):
         try:
             n = item.find("a", {"class": "search-item-doctor-link"})
             a = n.get('href')
-            print("https://www.ratemds.com/"+a)
-
+            
             #Obtain physician url for further parsing
-            doctor_url = "https://www.ratemds.com/" + a
+            doctor_url = "https://www.ratemds.com" + a
+            print("https://www.ratemds.com"+a)
 
             #Filter on all attributes containing class="rating comment"
-            #***This can be refactored to a function
+            #***This can be refactored (function use and chaining to make code clean)
             r = requests.get(doctor_url)
             c = r.content
             soup = BeautifulSoup(c, 'html.parser')
+            # import pdb; pdb.set_trace()
             ratings_text_list = soup.find_all(attrs={"class":"rating-comment-body"})
             ratings_date_list = soup.find_all(attrs={"class":"link-plain"})
-            for rating.text in ratings_list:
-                print(rating)
-                clean_rating = re.compile(r'(.+)(\\nWas this rating useful\? 0 flag)( \|.+)')
-                #Matches the first half of the text r'.+(?=Was this rating useful\? 0 flag)')
+            
+            for ratings_text_list.text in ratings_text_list:
+                print(ratings_text_list.text)
+
+            #***Make reviews and dates more readable on screen    
+            #Only parse information when called from database/dictionary
+            #     prog = re.compile(r'(.+)(\\nWas this rating useful\? 0 flag)( \|.+)',)
+            #     #Matches the first half of the text r'.+(?=Was this rating useful\? 0 flag)')
+            #     result = prog.match(rating.text)
+            #     print(result)
+            #     last_rating = rating.text
+                
 
             #Remove flag and react link from rating to get clean list
 
 
         except:
-            pass
             print("Looped stopped due to error.")
 
 ###########################
