416adff776a6cb836006e7e8b885814b275b146c
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-05-26 09:18:16 +0000

    Updated code and now displaying as array of reviews in template using REACT.

--- static/doctor-display.jsx
+++ static/doctor-display.jsx
@@ -19,28 +19,41 @@ class Reviews extends React.Component {
                 console.log(res);
                 return res.json();
             })
-            .then(reviews_list => {
-                console.log(reviews_list);
-                this.setState({reviews:reviews_list.review_text_body});
-            });
-    }
-
-    
+            .then(reviews_list => this.setState({reviews: reviews_list}));
+      //           // this.setState({todos: this.state.todos.map(todo=>{
+      // if (todo.id === id){
+      //   todo.completed = !todo.completed //toggles
+      // }
+      // return todo;
+            }
     // reviewDisplay = (review) => {
     //     console.log("In reviewDisplay function")
     //     console.log(review)
     // }
 
     render() {
+        const { reviews } = this.state
+
         return (
             <div>
-                <h1>Reviews</h1> 
-                    <p>{this.state.reviews}</>
-            </div>
-        );
-    }
+            Reviews 
+                    <ul> 
+                        {this.state.reviews.reviews
+
+                            // .map(review => 
+                            // <li key = {review.review_id}>
+                            // {reviews.review_text_body}
+                            // </li>
+                        }
+                        </ul>
+                    );
+            
+                    </div>
+                )
  
-}//end Review class
+            }
+        }
+//end Review class
 
 ReactDOM.render(
     <Reviews />,
