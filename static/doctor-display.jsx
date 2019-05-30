b2dcd1a400faa34a8c732fce79473a3f5fcfe684
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-05-30 00:23:36 +0000

    REACT component working for Reviewsgit add .

--- static/doctor-display.jsx
+++ static/doctor-display.jsx
@@ -3,21 +3,35 @@
 //Use event listener to use display provider info
 /* class start */
 class Reviews extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
-        this.state = {reviews: [""]};
+        this.state = {reviews: []};
+
+
+        // this.handleGuess = this.handleGuess.bind(this);
+        //Make sure to bind all methods that update state
+        this.updateReviews = this.updateReviews.bind(this);
 }
+    updateReviews(reviews){
+        // update state
+        return this.setState({reviews: reviews});
+    }
 
     componentDidMount() {
-
+        var that = this;
         console.log("In Component Mount lifecycle method!");
         fetch('/reviews')
             .then(res => {
                 console.log(res);
                 return res.json();
             })
-            .then(reviews_list => this.setState({reviews:reviews_list}));
+            // .then(reviews_list => {debugger; this.setState({reviews:reviews_list}) ));
+            .then(function(resp){
+                that.updateReviews(resp.reviews);
+                
+            });
+
       //           // this.setState({todos: this.state.todos.map(todo=>{
       // if (todo.id === id){
       //   todo.completed = !todo.completed //toggles
@@ -31,14 +45,13 @@ class Reviews extends React.Component {
 
     render() {
         debugger;
-        const { reviews } = this.state;
-        const reviewsList  = this.state.reviews.reviews;
-        console.log(reviewsList);
+        let reviews = this.state.reviews.map(review => {
+                    return <h1> {review} </h1>});
         return (
             <div>
                 Reviews 
                 <ol> 
-                    <li>{this.state.reviews.reviews}</li>
+                    <li>{reviews}</li>
                 </ol>
                 <ul>
                     <li>test1</li>
@@ -46,9 +59,9 @@ class Reviews extends React.Component {
             </div>
                 )
  
-            }
+            
 }
-  
+  }
     
 //end Review class
 
